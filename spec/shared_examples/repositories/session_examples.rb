7f35df7e1bc86743d2de4a95ee96f03b6ead9c52
Zack Danger Brown <zackdangerbrown@gmail.com>
2016-02-17 17:29:55 -0500

    Refactor contract_repository in active_record
    * added Factory Girl
    * refactored create on active_record_repository
    * test coverage for find_all_by_date

--- spec/shared_examples/repositories/session_examples.rb
+++ spec/shared_examples/repositories/session_examples.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 shared_examples "contract model" do
 
   context 'interface' do
@@ -7,7 +9,9 @@ shared_examples "contract model" do
   end
 
   context 'persisting an object' do
-    subject {Repository.for(:contract)}
+
+    subject { Repository.for(:contract) }
+
     it 'should create a new contract object with rate set to 100' do
       expect(subject.create({rate: 100}).rate).to eq(100)
     end
@@ -18,6 +22,21 @@ shared_examples "contract model" do
 
     it 'should create a new contract object with a photographer id of 1' do
       expect(subject.create({photographer_id: 1}).photographer_id).to eq(1)
+    end
+  end
+
+  context 'find all contracts by date' do
+
+    subject { Repository.for(:contract) }
+
+    before { FactoryGirl.create_list(:contract, 3) }
+
+    it 'should find all contracts by date' do
+      subject.find_all_by_date(Date.tomorrow).each.with_index {|contract, index|
+        expect(contract.photographer_id).to eq(1)
+        expect(contract.wedding_date).to eq(Date.tomorrow)
+        expect(contract.rate).to eq(100)
+      }
     end
   end
 
